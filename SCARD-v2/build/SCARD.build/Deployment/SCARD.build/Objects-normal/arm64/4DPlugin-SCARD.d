bc971dc196186bb3c0df0f34267899f522d9eb45
Keisuke Miyako <keisuke.miyako@4D.com>
2022-11-23 01:30:21 +0900

    apple silicon

---
+++ SCARD-v2/build/SCARD.build/Deployment/SCARD.build/Objects-normal/arm64/4DPlugin-SCARD.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4DPlugin-SCARD.cpp \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4DPlugin-SCARD.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/4DPluginAPI.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/Flags.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/PublicTypes.h \
+  include/wintypes.h include/libusb-1.0/libusb.h include/nfc/nfc.h \
+  include/nfc/nfc-types.h include/freefare.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/support/4DPlugin-JSON.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/Classes/C_BLOB.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/Classes/C_TYPES.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-scard-v2/SCARD-v2/4D\ Plugin\ API/Classes/C_TEXT.h
